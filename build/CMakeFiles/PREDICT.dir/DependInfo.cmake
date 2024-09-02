26e5b2790a0a7ceb92e93702cfc08419b1315dee
Mohsen Safaei <85816278+mohsensafaei@users.noreply.github.com>
2024-09-02 20:14:15 +0330

    Executive file

---
+++ build/CMakeFiles/PREDICT.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/Mohsen/Downloads/Compressed/NOJAN/src/ClogiPy.cpp" "CMakeFiles/PREDICT.dir/src/ClogiPy.cpp.o" "gcc" "CMakeFiles/PREDICT.dir/src/ClogiPy.cpp.o.d"
+  "/mnt/c/Users/Mohsen/Downloads/Compressed/NOJAN/src/Prediction.cpp" "CMakeFiles/PREDICT.dir/src/Prediction.cpp.o" "gcc" "CMakeFiles/PREDICT.dir/src/Prediction.cpp.o.d"
+  "/mnt/c/Users/Mohsen/Downloads/Compressed/NOJAN/src/main.cpp" "CMakeFiles/PREDICT.dir/src/main.cpp.o" "gcc" "CMakeFiles/PREDICT.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PREDICT.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PREDICT.dir/src/ClogiPy.cpp.o"
+  "CMakeFiles/PREDICT.dir/src/ClogiPy.cpp.o.d"
+  "CMakeFiles/PREDICT.dir/src/Prediction.cpp.o"
+  "CMakeFiles/PREDICT.dir/src/Prediction.cpp.o.d"
+  "CMakeFiles/PREDICT.dir/src/main.cpp.o"
+  "CMakeFiles/PREDICT.dir/src/main.cpp.o.d"
+  "PREDICT"
+  "PREDICT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PREDICT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
